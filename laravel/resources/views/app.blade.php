b5a0c5a11f9544dcda4ee2e150290c3bca5dd6d8
Kuroosh Patel <kurooshptl@gmail.com>
2021-07-05 19:53:29 +0530

    Added social media links

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -35,9 +35,10 @@
             <div class="row justify-content-between align-items-center">
                 <div class="col-lg-2 col-md-4">
                     <div class="header-top-socials text-center text-lg-left text-md-left">
-                        <a href="https://www.facebook.com/themefisher" target="_blank"><i class="ti-facebook"></i></a>
-                        <a href="https://twitter.com/themefisher" target="_blank"><i class="ti-twitter"></i></a>
-                        <a href="https://github.com/themefisher/" target="_blank"><i class="ti-github"></i></a>
+                        <a href="{{config('constants.facebook_url')}}" target="_blank"><i class="ti-facebook"></i></a>
+                        <a href="{{config('constants.instagram_url')}}" target="_blank"><i class="ti-instagram"></i></a>
+{{--                        <a href="https://github.com/themefisher/" target="_blank"><i class="ti-github"></i></a>--}}
+                        <a href="{{config('constants.linked_in_url')}}" target="_blank"><i class="ti-linkedin"></i></a>
                     </div>
                 </div>
                 <div class="col-lg-10 col-md-8 text-center text-lg-right text-md-right">
--- laravel/resources/views/layout/footer.blade.php
+++ laravel/resources/views/layout/footer.blade.php
@@ -59,9 +59,9 @@
                 </div>
                 <div class="col-lg-6 text-left text-lg-right">
                     <ul class="list-inline footer-socials">
-                        <li class="list-inline-item"><a href="https://www.facebook.com/themefisher"><i class="ti-facebook mr-2"></i>Facebook</a></li>
-                        <li class="list-inline-item"><a href="https://twitter.com/themefisher"><i class="ti-twitter mr-2"></i>Twitter</a></li>
-                        <li class="list-inline-item"><a href="https://www.pinterest.com/themefisher/"><i class="ti-linkedin mr-2 "></i>Linkedin</a></li>
+                        <li class="list-inline-item"><a href="{{config('constants.facebook_url')}}" target="_blank"><i class="ti-facebook mr-2"></i>Facebook</a></li>
+                        <li class="list-inline-item"><a href="{{config('constants.instagram_url')}}" target="_blank"><i class="ti-instagram mr-2"></i>Instagram</a></li>
+                        <li class="list-inline-item"><a href="{{config('constants.linked_in_url')}}" target="_blank"><i class="ti-linkedin mr-2 "></i>Linkedin</a></li>
                     </ul>
                 </div>
             </div>
