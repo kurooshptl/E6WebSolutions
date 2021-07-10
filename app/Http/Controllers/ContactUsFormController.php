c5413382b8e04829cec07500c12b4bb23ffa4a63
Kuroosh Patel <kurooshptl@gmail.com>
2021-07-10 20:33:32 +0530

    html changes

---
+++ app/Http/Controllers/ContactUsFormController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\ContactUsRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ * Class ContactUsFormController
+ * @package App\Http\Controllers
+ */
+class ContactUsFormController extends Controller
+{
+    /**
+     * @var ContactUsRepository
+     */
+    public $contactUsRepository;
+
+    /**
+     * ContactUsFormController constructor.
+     *
+     * @param ContactUsRepository $contactUsRepository
+     */
+    public function __construct(ContactUsRepository $contactUsRepository)
+    {
+
+        $this->contactUsRepository = $contactUsRepository;
+    }
+
+    // Store Contact Form data
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+
+        try {
+            // Form validation
+            $validator = Validator::make($request->all(), [
+                'name'    => 'required',
+                'email'   => 'required|email',
+                'phone'   => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+                'message' => 'required'
+            ]);
+            if ($validator->fails()) {
+//                return Redirect::back()->withInput()->withErrors($validator);
+                return "Please enter correct values";
+            }
+
+            //  Store data in database
+//        Contacts::create($request->all());
+
+            $this->contactUsRepository->store($request->all());
+
+            //  Send mail to admin
+            \Mail::send('emails.contact', array(
+                'name'       => $request->get('name'),
+                'email'      => $request->get('email'),
+                'phone'      => $request->get('phone'),
+                'user_query' => $request->get('message'),
+            ), function ($message) use ($request) {
+
+                $message->from($request->email);
+                $message->to('e6websolutions@gmail.com', 'E6WebSolutions')->subject('E6WebSolution Contact Mail');
+            });
+
+            //
+//            return back()->with('success', 'We have received your message and would like to thank you for writing to us.');
+            return "OK";
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+    }
+
+    public function subscribe(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email'   => 'required|email'
+        ]);
+        if ($validator->fails()) {
+           return "Please enter email id";
+        }
+    }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E6 Web Solutions</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{asset('img/E6_Logo.png')}}" rel="icon">
+    <link href="{{asset('img/apple-touch-icon.png" rel="')}}apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="{{asset('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i')}}" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: Arsha - v4.3.0
+    * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top header-scrolled">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo me-auto"><a href="{{route('home')}}">{{env('APP_NAME')}}</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo me-auto"><img src="img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="{{route('home')}}">Home</a></li>
+                <li><a class="nav-link scrollto" href="{{route('about')}}">About</a></li>
+                <li><a class="nav-link scrollto" href="{{route('service')}}">Services</a></li>
+{{--                <li><a class="nav-link   scrollto" href="{{route('portfolio')}}">Portfolio</a></li>--}}
+                <li><a class="nav-link scrollto" href="{{route('blog')}}">Blog</a></li>
+                <li><a class="nav-link scrollto" href="{{route('team')}}">Team</a></li>
+
+                <li><a class="nav-link scrollto" href="{{route('contact')}}">Contact</a></li>
+{{--                <li><a class="getstarted scrollto" href="#about">Get Quote</a></li>--}}
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
+
+<!-- Header Close -->
+<main id="main">
+
+@yield('content')
+
+</main>
+@extends('layout.footer')
+</div>
+@extends('layout.scripts')
+
+</body>
+</html>
